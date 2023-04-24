0fa382d449db78f140be523ccc82cce34d5c3625
Chiragsolanki843 <Chiragsolanki843@gmail.com>
2023-04-24 14:07:58 +0530

    Twitter Project

--- src/main/scala/part5twitter/CustomReceiverApp.scala
+++ src/main/scala/part5twitter/CustomReceiverApp.scala
@@ -1,59 +1,59 @@
-package part5twitter
-
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.storage.StorageLevel
-import org.apache.spark.streaming.dstream.DStream
-import org.apache.spark.streaming.receiver.Receiver
-import org.apache.spark.streaming.{Seconds, StreamingContext}
-import java.net.Socket
-import scala.concurrent.{Future, Promise}
-import scala.io.Source
-
-class CustomSocketReceiver(host: String, port: Int) extends Receiver[String](StorageLevel.MEMORY_ONLY) {
-
-
-  // in production level "MEMORY_AND_DISK_2" we need to use because if some how data is not save in
-  // memory then all data will save in 'DISK' and replicated twice. '2' means replicated data twice so you
-  // don't loose data in cluster.
-  import scala.concurrent.ExecutionContext.Implicits.global
-
-  val socketPromise: Promise[Socket] = Promise[Socket]()
-  val socketFuture = socketPromise.future
-
-  // called asynchronously
-  override def onStart(): Unit = {
-    val socket = new Socket(host, port)
-
-    // run on another thread
-    Future {
-      Source.fromInputStream(socket.getInputStream)
-        .getLines()
-        .foreach(line => store(line)) // store makes this string available to Spark
-    }
-
-    socketPromise.success(socket)
-  }
-
-  // called asynchronously
-  override def onStop(): Unit = socketFuture.foreach(socket => socket.close())
-}
-
-object CustomReceiverApp {
-
-  val spark = SparkSession.builder()
-    .appName("Custom receiver app")
-    .master("local[*]")
-    .getOrCreate()
-
-  spark.sparkContext.setLogLevel("ERROR")
-
-  val ssc = new StreamingContext(spark.sparkContext, Seconds(1))
-
-  def main(args: Array[String]): Unit = {
-    val dataStream: DStream[String] = ssc.receiverStream(new CustomSocketReceiver("localhost", 12345))
-    dataStream.print()
-    ssc.start()
-    ssc.awaitTermination()
-  }
-
-}
+//package part5twitter
+//
+//import org.apache.spark.sql.SparkSession
+//import org.apache.spark.storage.StorageLevel
+//import org.apache.spark.streaming.dstream.DStream
+//import org.apache.spark.streaming.receiver.Receiver
+//import org.apache.spark.streaming.{Seconds, StreamingContext}
+//import java.net.Socket
+//import scala.concurrent.{Future, Promise}
+//import scala.io.Source
+//
+//class CustomSocketReceiver(host: String, port: Int) extends Receiver[String](StorageLevel.MEMORY_ONLY) {
+//
+//
+//  // in production level "MEMORY_AND_DISK_2" we need to use because if some how data is not save in
+//  // memory then all data will save in 'DISK' and replicated twice. '2' means replicated data twice so you
+//  // don't loose data in cluster.
+//  import scala.concurrent.ExecutionContext.Implicits.global
+//
+//  val socketPromise: Promise[Socket] = Promise[Socket]()
+//  val socketFuture = socketPromise.future
+//
+//  // called asynchronously
+//  override def onStart(): Unit = {
+//    val socket = new Socket(host, port)
+//
+//    // run on another thread
+//    Future {
+//      Source.fromInputStream(socket.getInputStream)
+//        .getLines()
+//        .foreach(line => store(line)) // store makes this string available to Spark
+//    }
+//
+//    socketPromise.success(socket)
+//  }
+//
+//  // called asynchronously
+//  override def onStop(): Unit = socketFuture.foreach(socket => socket.close())
+//}
+//
+//object CustomReceiverApp {
+//
+//  val spark = SparkSession.builder()
+//    .appName("Custom receiver app")
+//    .master("local[*]")
+//    .getOrCreate()
+//
+//  spark.sparkContext.setLogLevel("ERROR")
+//
+//  val ssc = new StreamingContext(spark.sparkContext, Seconds(1))
+//
+//  def main(args: Array[String]): Unit = {
+//    val dataStream: DStream[String] = ssc.receiverStream(new CustomSocketReceiver("localhost", 12345))
+//    dataStream.print()
+//    ssc.start()
+//    ssc.awaitTermination()
+//  }
+//
+//}
--- src/main/scala/part5twitter/TwitterReceiver.scala
+++ src/main/scala/part5twitter/TwitterReceiver.scala
@@ -1,45 +1,58 @@
-package part5twitter
-
-import org.apache.spark.storage.StorageLevel
-import org.apache.spark.streaming.receiver.Receiver
-import twitter4j._
-
-import scala.concurrent.Promise
-
-class TwitterReceiver extends Receiver[Status](StorageLevel.MEMORY_ONLY) {
-
-  import scala.concurrent.ExecutionContext.Implicits.global
-
-  val twitterStreamPromise = Promise[TwitterStream]
-  val twitterStreamFuture = twitterStreamPromise.future
-
-  def simpleStatusListener = new StatusListener {
-    override def onStatus(status: Status): Unit = store(status)
-
-    override def onDeletionNotice(statusDeletionNotice: StatusDeletionNotice): Unit = ()
-
-    override def onTrackLimitationNotice(numberOfLimitedStatuses: Int): Unit = ()
-
-    override def onScrubGeo(userId: Long, upToStatusId: Long): Unit = ()
-
-    override def onStallWarning(warning: StallWarning): Unit = ()
-
-    override def onException(ex: Exception): Unit = ex.printStackTrace()
-  }
-
-  // run asynchronously
-  override def onStart(): Unit = {
-    val twitterStream: TwitterStream = new TwitterStreamFactory("src/main/resources")
-      .getInstance()
-      .addListener(simpleStatusListener)
-      .sample("english") // call the Twitter sample endpoint for English tweets
-
-    twitterStreamPromise.success(twitterStream)
-  }
-
-  // run asynchronously
-  override def onStop(): Unit = twitterStreamFuture.foreach { twitterStream =>
-    twitterStream.cleanUp()
-    twitterStream.shutdown()
-  }
-}
+//package part5twitter
+//
+//import org.apache.spark.storage.StorageLevel
+//import org.apache.spark.streaming.receiver.Receiver
+//import twitter4j._
+//
+//import java.io.{PrintStream, OutputStream}
+//import scala.concurrent.Promise
+//import scala.sys.process.processInternal.OutputStream
+//
+//class TwitterReceiver extends Receiver[Status](StorageLevel.MEMORY_ONLY) {
+//
+//  import scala.concurrent.ExecutionContext.Implicits.global
+//
+//  val twitterStreamPromise = Promise[TwitterStream]
+//  val twitterStreamFuture = twitterStreamPromise.future
+//
+//  def simpleStatusListener = new StatusListener {
+//    override def onStatus(status: Status): Unit = store(status)
+//
+//    override def onDeletionNotice(statusDeletionNotice: StatusDeletionNotice): Unit = ()
+//
+//    override def onTrackLimitationNotice(numberOfLimitedStatuses: Int): Unit = ()
+//
+//    override def onScrubGeo(userId: Long, upToStatusId: Long): Unit = ()
+//
+//    override def onStallWarning(warning: StallWarning): Unit = ()
+//
+//    override def onException(ex: Exception): Unit = ex.printStackTrace()
+//  }
+//
+//  private def redirectSystemError() = System.setErr(new PrintStream(new OutputStream {
+//    override def write(b: Array[Byte]): Unit = ()
+//
+//    override def write(b: Array[Byte], off: Int, len: Int): Unit = ()
+//
+//    override def write(b: Int): Unit = ()
+//  }))
+//
+//  // run asynchronously
+//  override def onStart(): Unit = {
+//
+//    redirectSystemError()
+//
+//    val twitterStream: TwitterStream = new TwitterStreamFactory("src/main/resources")
+//      .getInstance()
+//      .addListener(simpleStatusListener)
+//      .sample("english") // call the Twitter sample endpoint for English tweets
+//
+//    twitterStreamPromise.success(twitterStream)
+//  }
+//
+//  // run asynchronously
+//  override def onStop(): Unit = twitterStreamFuture.foreach { twitterStream =>
+//    twitterStream.cleanUp()
+//    twitterStream.shutdown()
+//  }
+//}
